17030263da7f99fba21a3f519761ba9cbfee868a
John <gongzhaoman@outlook.com>
2024-05-11 21:48:46 +0800

    chore: add shadcnUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 function App() {
-  return <main className="flex h-screen flex-col items-center justify-center">hello world</main>
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <Button>Click me</Button>
+      hello world
+    </div>
+  )
 }
 
 export default App
